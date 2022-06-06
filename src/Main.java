9a96efeaa4a070db066657fd3ade343be9662ecb
Mikhail9845 <mihail007.08@mail.ru>
2022-06-06 20:32:58 +0300

    File is changed

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+      int amount = 350;
+      int rubles = 20;
+      int miles = amount / rubles;
+      if (amount >= 20){
+          System.out.println("Ваше количество миль = " + miles);
+      }else {
+          System.out.println("У вас нет миль");
+      }
+
+    }
+
+    }
